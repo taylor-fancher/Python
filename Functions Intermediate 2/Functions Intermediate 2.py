b45bfc7a0cc6935f6785ce4cca565a5c1ec2b554
Taylor Fancher <tfanch24@gmail.com>
2021-06-09 16:16:42 -0500

    added dictionaries

--- Functions Intermediate 2/Functions Intermediate 2.py
+++ Functions Intermediate 2/Functions Intermediate 2.py
@@ -10,6 +10,59 @@ sports_directory = {
 }
 z = [ {'x': 10, 'y': 20} ]
 
-def update(list):
-    x[1[0]] = list[0]
-    students[1]
+#2 Iterate Through a List of Dictionaries
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary():
+    pass
+# iterateDictionary(students) should output:
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+#3 Get Values From a List of Dictionaries
+def iterateDictionary2():
+    pass
+# iterateDictionary2('last_name', students) should output
+# Michael
+# John
+# Mark
+# KB
+
+# iterateDictionary2('last_name', students) should output
+# Jordan
+# Rosales
+# Guillen
+# Tonel
+
+
+#4 Iterate Through a Dictionary with List Values
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+# printInfo(dojo) should output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
