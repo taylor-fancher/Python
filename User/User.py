b7f5f6963cfcd05a0fdaeb4e479f516a289b98f1
Taylor Fancher <tfanch24@gmail.com>
2021-06-20 16:17:51 -0500

    intial commit

---
+++ User/User.py
@@ -0,0 +1,16 @@
+class user:
+    def __init__(self, username):
+        self.name = username
+        self.account_balance = 0
+    def make_deposit(self, amount):
+        self.account_balance += amount
+    def make_withdrawl(self, amount):
+        self.account_balance -= amount
+    def display_user_balance(self):
+        print(self.account_balance)
+    def transfer_money(self, other_user, amount):
+        self.account_balance -= amount
+        other_user.account_balance += amount
+
+Paul = user('Paul')
+Paul.display_user_balance()
